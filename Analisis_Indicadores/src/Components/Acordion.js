8c527e7e91ed64510b0840f8e813103b167993e8
fabio saez <the_fabio@live.cl>
2022-04-22 20:36:15 -0400

    porcentaje color

--- Analisis_Indicadores/src/Components/Acordion.js
+++ Analisis_Indicadores/src/Components/Acordion.js
@@ -7,12 +7,35 @@ const Acordion = (props) => {
 
   const [indi, setIndi] = useState(props.indicadores)
 
+  const style = {
+    margin: '0.5em',
+    paddingLeft: 0,
+    listStyle: 'none',
+  };
+  const style0 = {
+    color: "red"
+  }
+  const style10= {
+    color: "green"
+  }
+
+  const calcularColor = (porcentaje) => {
+    let porcentajeInt = parseInt(porcentaje);
+    if (porcentajeInt <50){
+    return "style0" }
+    else return "style10"
+  }
+
   return (
-    <div>
-        <Accordion >
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>{props.nombre} - {props.porcentaje}</Accordion.Header>
-          <Accordion.Body>
+    <div >
+        <Accordion style={style}>
+        <Accordion.Item eventKey="0" >
+          <Accordion.Header >
+            
+            {props.nombre}
+            <h3 className={parseInt(props.porcentaje) <= 50 ? "porcentaje style0" : "porcentaje style10"}> {props.porcentaje}% </h3>    
+          </Accordion.Header>
+          <Accordion.Body >
             {indi.map((indicador) => {
               return(
                 <div key={indicador.id}>
